3cd4e75be6f9fef348965840bdd9973b0cdd7ea9
manmanmeer <masak6161@gmail.com>
2024-06-22 20:39:27 +0900

    Report画面　レイアウト

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,8 +1,32 @@
+import { Grid, Paper } from '@mui/material'
 import React from 'react'
+import CategoryChart from '../components/layout/CategoryChart';
+import MonthSelector from '../components/layout/MonthSelector';
+import BarChart from '../components/layout/BarChart';
+import TransactionTable from '../components/layout/TransactionTable';
 
 const Report = () => {
+  const commonPaperStyle = {
+    height: { xs: "auto", md: "400px" },
+    display: "flex",
+    flexDirection: "column",  
+  };
+
   return (
-    <div>Report</div>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <MonthSelector />
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Paper sx={commonPaperStyle}><CategoryChart /></Paper>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Paper sx={commonPaperStyle}><BarChart /></Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <TransactionTable />
+      </Grid>
+    </Grid>
   )
 }
 
