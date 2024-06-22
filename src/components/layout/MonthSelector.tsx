e05af69042733ed8ae9bc1afc0a5da243bc08ab6
manmanmeer <masak6161@gmail.com>
2024-06-22 20:59:45 +0900

    DatePicker

--- src/components/layout/MonthSelector.tsx
+++ src/components/layout/MonthSelector.tsx
@@ -1,8 +1,23 @@
+import { Box, Button } from '@mui/material'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 import React from 'react'
 
 const MonthSelector = () => {
   return (
-    <div>MonthSelector</div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <Box 
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: "center"  }}
+    >
+        <Button color={"error"} variant='contained'>
+            先月
+        </Button>
+        <DatePicker sx={{ mx: 2 }}/>
+        <Button color={"primary"} variant='contained'>
+            次月
+        </Button>
+    </Box>
+    </LocalizationProvider>
   )
 }
 
